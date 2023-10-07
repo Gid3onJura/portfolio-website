22a107737f9a24ae9fe2451b6ac2f89a3225dd04
Gid3onJura <homebasekorlash@googlemail.com>
2023-10-07 10:39:57 +0200

    simple timeline, visibility bug

--- components/experience.tsx
+++ components/experience.tsx
@@ -4,14 +4,44 @@ import React from "react"
 import SectionHeading from "./sectionheading"
 import { useSectionInView } from "@/lib/hooks"
 import { motion } from "framer-motion"
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
+import { experiencesData } from "@/lib/data"
 
 export default function Experience() {
-  const { ref } = useSectionInView("Erfahrung")
+  // const { ref } = useSectionInView("Erfahrung")
 
   return (
-    <section ref={ref} id="experience" className="scroll-mt-28 max-w-[53rem] text-center sm:mb-40">
+    <section id="experience">
       <SectionHeading>Meine Erfahrungen</SectionHeading>
-      sdsdsdsd sdsdsdsd sdsdsdsd sdsdsdsd sdsdsdsd sdsdsdsd sdsdsdsd sdsdsdsd sdsdsdsd sdsdsdsd
+      <VerticalTimeline>
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgba(0,0,0,0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af",
+              }}
+              date={item.date}
+              icon={item.icon}
+              iconStyle={{
+                background: "white",
+                fontSize: "1.5rem",
+              }}
+            >
+              <h3>{item.title}</h3>
+              <p>{item.location}</p>
+              <p>{item.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </section>
   )
 }
